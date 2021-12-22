f8d6ac1924923244705aea207c9a04d3b4590cea
manojkumar1053 <manojkumar1053@gmail.com>
2021-12-21 20:26:46 -0500

    click command test

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import click
 
+
 @click.command(help="This is just a hello app. It does nothing.")
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
@@ -8,7 +9,9 @@ def hello(name, color):
         click.echo("Thor, you are always red.")
         click.echo(click.style(f"Hello {name}!", fg="red"))
     else:
-        pass
+        click.echo(f"Your color is {color}!")
+        click.echo(click.style(f"Hello {name}!", fg=color))
+
 
 if __name__ == "__main__":
     hello()
